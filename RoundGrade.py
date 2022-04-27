2e910d670150ed85a0b70715f5f8db963f6a2994
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-04-27 21:59:30 +0200

    Update RoundGrade.py

--- RoundGrade.py
+++ RoundGrade.py
@@ -2,28 +2,27 @@
 """
 Created on Wed Apr 27 18:17:39 2022
 
-@author: willi
+@author: William H. Langvad
 """
 
 import numpy as np
 import os
 
-gradeType = np.array([-3,0,2,4,7,10,12])
+gradeType = np.array([-3,0,2,4,7,10,12]) #Karakterskalaen defineres
 
 def roundGrade(grades):
-    l = np.size(grades)
-    gradesRounded = np.zeros(l)
+    l = np.size(grades) #antallet af karakterer i grades defineres
+    gradesRounded = np.zeros(l) #en vektor oprettes med et antal 0'er tilsvarende l
     
-    for i in range(l):
-        for j in range(6):
-            lower = gradeType[j]
+    for i in range(l): #koden behandler hver karakter enkeltvist
+        for j in range(6): #koden kigger på tal som ligger mellem to karakterertrin af gangen.
+            lower = gradeType[j] #intervaller til at sortere karakteren defineres
             upper = gradeType[j+1]
             middle = gradeType[j] + (gradeType[j+1]-gradeType[j])/2
             
-            if grades[i] >= lower and grades[i] < middle:
+            if grades[i] >= lower and grades[i] < middle: #hvis koden er mellem nedre grændse og midten af intervallet, rundes der ned
                 gradesRounded[i] = gradeType[j]
-            if grades[i] >= middle and grades[i] <= upper:
+            if grades[i] >= middle and grades[i] <= upper: #hvis koden er mellem øvre grændse og midten af intervallet, rundes der op
                 gradesRounded[i] = gradeType[j+1]
-
     
     return gradesRounded
