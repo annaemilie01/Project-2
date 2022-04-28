4c365511ae6493b1da057e3c73e0bc9caac670de
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-04-28 10:37:26 +0200

    Add files via upload

---
+++ plots.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 10:07:06 2022
+
+@author: william 
+"""
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+gradeType = np.array([-3,0,2,4,7,10,12]) #Karakterskalaen defineres
+
+def gradesPlot(grades):
+    
+    occ = np.zeros(7)   #en vektor skabes med syv 0'er tilsvarende de syv karaktertrin
+    for i in range(7):
+        occ[i] = np.size(grades[grades==gradeType[i]]) #hyppigheden af hver karakter findes
+    
+    #plot 1 skabes
+    plt.bar(gradeType,occ)
+    plt.title('Final Grades')
+    plt.xlabel('Grade Type')
+    plt.ylabel('Occurrence')
+    plt.show()
+
