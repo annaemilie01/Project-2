28918d225dedf86f7013c64d14ba56f2644b6a85
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-05-07 16:09:46 +0200

    Add files via upload

---
+++ checkErrors.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  7 14:59:12 2022
+
+@author: willi
+"""
+
+
+import os
+import numpy as np
+import pandas
+from FileReader import readData
+import matplotlib.pyplot as plt
+import time
+
+def ErrorCheck(StudentNumbers,Grades):
+    gradeType = [-3,0,2,4,7,10,12] #Karakterskalaen defineres
+    ls = np.size(StudentNumbers)
+    lg = np.size(Grades[:,0])
+    wg = np.size(Grades[0,:])
+    errorCount = 0
+    
+    for i in range(ls):
+        for j in range(1,ls-i):    
+           if StudentNumbers[i] == StudentNumbers[i+j]:
+               print("Fejl fundet i datasæt")
+               print("Studienummeret " + str(StudentNumbers[i]) + " optræder flere steder")
+               errorCount = errorCount + 1
+               
+    for i in range(lg):
+        for j in range(wg):
+            if  Grades[i,j] not in gradeType:
+                print("Fejl fundet i datasæt" )
+                print(str(Grades[i,j]) + " er ikke en gyldig karakter")
+                errorCount = errorCount + 1
+    print("")
+    print("Der er fundet " + str(errorCount) + " fejl i alt")
+
+
+    
