f636e67d8c1bb1f73b76fd6d8163767cd57c27c0
Will1364 <97221309+Will1364@users.noreply.github.com>
2022-05-12 14:30:16 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,28 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 28 10:07:06 2022
 
-@author: william 
-"""
-import numpy as np
-import os
-import matplotlib.pyplot as plt
 
-gradeType = np.array([-3,0,2,4,7,10,12]) #Karakterskalaen defineres
-
-def gradesPlot(grades):
-    
-    occ = np.zeros(7)   #en vektor skabes med syv 0'er tilsvarende de syv karaktertrin
-    for i in range(7):
-        occ[i] = np.size(grades[grades==gradeType[i]]) #hyppigheden af hver karakter findes
-    
-    #plot 1 skabes
-    plt.bar(gradeType,occ)
-    plt.title('Final Grades')
-    plt.xlabel('Grade Type')
-    plt.ylabel('Occurrence')
-    plt.show()
-############################################################################################################
 def gradesPlot(Grades,Names,Assignments):
     
     
@@ -69,15 +46,15 @@ def gradesPlot(Grades,Names,Assignments):
     xR = np.array(x + [np.random.uniform(-0.1,0.1) for i in range(len(x))])
     yR = np.array(y + [np.random.uniform(-0.1,0.1) for i in range(len(y))])
     
-    AsMean = np.zeros(Ass)
+    Mean = np.zeros(Ass)
     
 
-    #for i in range(Ass):
-        #AsMean[i] = np.mean(np.array([:,i]))
+    for i in range(Ass):
+        Mean[i] = np.mean(np.array(Ass[:,i]))
     
     #Definerer og bestemmet plottet
     plt.plot(xR,yR,"o")
-    plt.plot(As,AsMean)
+    plt.plot(As,Mean)
     plt.title("Grades per assignment", color = "black")
     plt.xlabel("Assignment number")
     plt.ylabel("Grade")    
@@ -86,3 +63,4 @@ def gradesPlot(Grades,Names,Assignments):
     plt.yticks(y,color = "Green")
     plt.ylim([-3.5, np.max(yR)+4])     
     plt.show()
+
