d15d4a66eb85157741530cf5f532110473686d4b
annaemilie01 <102285599+annaemilie01@users.noreply.github.com>
2022-05-12 13:24:45 +0200

    Update plots.py
    Sejt

--- plots.py
+++ plots.py
@@ -22,4 +22,67 @@ def gradesPlot(grades):
     plt.xlabel('Grade Type')
     plt.ylabel('Occurrence')
     plt.show()
+############################################################################################################
+def gradesPlot(Grades,Names,Assignments):
+    
+    
+    occ = np.zeros(7)   #en vektor skabes med syv 0'er tilsvarende de syv karaktertrin
+    for i in range(7):
+        occ[i] = np.size(Grades[Grades==gradeType[i]]) #hyppigheden af hver karakter findes
+    langs=['-3','0','2','4','7','10','12']     #Bestemmer x-aksen
+    
+    plt.bar(langs,occ,color=['Green'])
+    plt.title('Final Grades')
+    plt.xlabel('Grade Type')
+    plt.ylabel('Occurrence')
+    plt.show()
 
+
+# Plot 2: Grades per Assignment
+    
+        #Definerer antal elever og antal opgaver
+    Elever = np.shape(Names)[0]
+    Ass = np.shape(Assignments)[1]
+    
+        #Definerer et loop for de mange antal opgaver   
+    As = np.zeros(Ass)
+    for i in range(Ass):
+        As[i] = i+1
+
+
+    x = np.array([])
+    
+    #Laver et loop der sørger for at der er giver opgavere numre ift. antal studerende
+    for i in range(Ass):
+        
+        AsX = np.ones(Elever)*(i+1)
+        x = np.append([x],[AsX])
+      
+    y = np.array([])
+    
+    # Et loop der giver hver opgave karakter på y aksen
+    for i in range(Ass):
+        
+        y = np.append([y],[np.array(Grades[:,i])])
+    
+    #Lægger et tilfældigt tal mellem (-0.1,0.1) til x- og y koordinat
+    xR = np.array(x + [np.random.uniform(-0.1,0.1) for i in range(len(x))])
+    yR = np.array(y + [np.random.uniform(-0.1,0.1) for i in range(len(y))])
+    
+    AsMean = np.zeros(Ass)
+    
+
+    #for i in range(Ass):
+        #AsMean[i] = np.mean(np.array([:,i]))
+    
+    #Definerer og bestemmet plottet
+    plt.plot(xR,yR,"o")
+    plt.plot(As,AsMean)
+    plt.title("Grades per assignment", color = "black")
+    plt.xlabel("Assignment number")
+    plt.ylabel("Grade")    
+    plt.legend(["Every assignment of every student", "Mean grade of each assignment"], loc ="upper left")
+    plt.xticks(x,color = "black")
+    plt.yticks(y,color = "Green")
+    plt.ylim([-3.5, np.max(yR)+4])     
+    plt.show()
